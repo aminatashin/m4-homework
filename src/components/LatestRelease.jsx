503d222857b025e4b082a760dbb1cd7907ab7979
Amin Atashkhayer <amin.atashin@gmail.com>
2022-03-15 17:47:41 +0100

    Update LatestRelease.jsx

--- src/components/LatestRelease.jsx
+++ src/components/LatestRelease.jsx
@@ -1,38 +1,34 @@
-import { Card,ListGroupItem , ListGroup, Container, Row, Col } from "react-bootstrap"
+import {
+  Card,
+  ListGroupItem,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
-import books from '../horror/horror.json'
+import books from "../horror/horror.json";
 
-const LatestRelease = ()=>{
-
-    return(
-        <Container >
-        <Row  className="justify-content-center" >
-        {books.map((book)=>(
+const LatestRelease = () => {
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        {books.map((book) => (
           <Col md={3}>
-              
-
-                  
-                    <Card className="mt-3">  
-                  <Card.Img variant="top" src={book.img}  />
-    <Card.Body>
-      <Card.Title>{book.title}</Card.Title>
-      <Card.Text>
-       {book.category}
-      </Card.Text>
-    </Card.Body>
-    <ListGroup className="list-group-flush">
-      <ListGroupItem>{book.price + "$"}</ListGroupItem>
-     
-    </ListGroup>
-   
-    </Card> </Col> ))}
-
-    
-  
- 
-        </Row>
-      </Container>
-    )
-}
-export default LatestRelease
-    
+            <Card className="mt-3">
+              <Card.Img variant="top" src={book.img} />
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <Card.Text>{book.category}</Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>{book.price + "$"}</ListGroupItem>
+              </ListGroup>
+            </Card>{" "}
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+export default LatestRelease;
